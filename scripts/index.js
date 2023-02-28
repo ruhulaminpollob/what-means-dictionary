73ea2e2b2f8935cfb21f828dbc0351985c83a872
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-28 13:15:17 +0600

    dictionary incompletely finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,50 @@
 const loadMeaning = (word) => {
     if (word) {
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    fetch(url)
-        .then(Response => Response.json())
-        .then(data => {
-            
-            if (data.title==="No Definitions Found") {
-                 alert(data.message)
-            } else {
-                displayMeaning(data)
-            }
-        });
-    }else{
+        fetch(url)
+            .then(Response => Response.json())
+            .then(data => {
+
+                if (data.title === "No Definitions Found") {
+                    alert(data.message)
+                } else {
+                    displayMeaning(data)
+                }
+            });
+    } else {
         alert('empty input field')
     }
-    
+
 
 }
 
-const displayMeaning=(wordInfo)=>{
+const displayMeaning = (wordInfo) => {
+    // meanings
+    const meaning = document.getElementById('meaning');
+    const synonyms = wordInfo[0].meanings[0].synonyms;
+    meaning.innerHTML = `
+    <p>${synonyms[0] ? synonyms[0] : wordInfo[0].word}</p>
+    `
+
+
     wordInfo[0].phonetics.forEach(element => {
-        const audioResult=document.getElementById('audio-result');
-        console.log(element.audio);
-        const audio=document.createElement('audio');
-        audio.src=element.audio
-        const audioPlayBtn=document.createElement('button');
-        audioPlayBtn.innerText='Play'
-        audioPlayBtn.onclick=()=>audio.play()
+        const audioResult = document.getElementById('audio-result');
+        // console.log(element.audio);
+        const audio = document.createElement('audio');
+        audio.src = element.audio
+        const audioPlayBtn = document.createElement('button');
+        audioPlayBtn.innerText = 'Play'
+        audioPlayBtn.onclick = () => audio.play()
         audioResult.appendChild(audio)
         audioResult.appendChild(audioPlayBtn)
-        
+
         // console.log(audio);
 
     });
+
+
+
+
     // console.log(wordInfo[0].phonetics);
 }
 
@@ -46,7 +58,7 @@ const displayMeaning=(wordInfo)=>{
 // search event handler 
 document.getElementById('search-btn').addEventListener('click', function () {
     const searchInputFiled = document.getElementById('search-input-filed').value;
-    
+
     loadMeaning(searchInputFiled)
 })
 
